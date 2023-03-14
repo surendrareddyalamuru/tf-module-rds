aaea98c228d260c40163995388dc6f371fd2dccd
surendra reddy <surendrareddylamuru01@gmail.com>
2023-03-14 11:40:08 +0530

    rds

---
+++ main.tf
@@ -0,0 +1,37 @@
+resource "aws_rds_cluster" "default" {
+  depends_on          = [aws_rds_cluster_parameter_group.default, aws_db_subnet_group.default]
+  cluster_identifier  = "${var.env}-${var.name}-roboshop-rds"
+  engine              = var.engine
+  engine_version      = var.engine_version
+  database_name       = "mydb"
+  master_username     = "foo"
+  master_password     = "xyz"
+  skip_final_snapshot = true
+  db_cluster_parameter_group_name = "${var.env}-${var.name}-roboshop-rds"
+  db_subnet_group_name   = "${var.env}-${var.name}-roboshop-rds"
+#  vpc_security_group_ids = [aws_security_group.sg.id]
+}
+
+resource "aws_rds_cluster_parameter_group" "default" {
+  name        = "${var.env}-${var.name}-roboshop-rds"
+  family      = "aurora5.6"
+  description = "RDS default cluster parameter group"
+}
+
+resource "aws_db_subnet_group" "default" {
+  name       = "${var.env}-${var.name}-roboshop-rds"
+  subnet_ids = var.subnets
+
+  tags = {
+    Name = "${var.env}-${var.name}-roboshop-rds"
+  }
+}
+
+#resource "aws_rds_cluster_instance" "cluster_instances" {
+#  for_each           = var.nodes
+#  identifier         = "${var.env}-${var.name}-rds-${each.key}"
+#  cluster_identifier = aws_rds_cluster.default.id
+#  instance_class     = each.value.instance_class
+#  engine             = aws_rds_cluster.default.engine
+#  engine_version     = aws_rds_cluster.default.engine_version
+#}
--- vars.tf
+++ vars.tf
@@ -1,5 +1,9 @@
-variable "rds" {}
-
 variable "env" {}
-
 variable "subnets" {}
+variable "allocated_storage"
+variable "engine" {}
+variable "engine_version" {}
+variable "instance_class" {}
+variable "skip_final_snapshot" {}
+variable "parameter_group_name" {}
+variable "name" {}
