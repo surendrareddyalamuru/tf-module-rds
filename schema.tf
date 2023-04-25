a389ffd7b72f1a7ad15ffaf5a94ee6365967c567
surendra reddy <surendrareddylamuru01@gmail.com>
2023-04-25 10:16:20 +0530

    rds

--- schema.tf
+++ schema.tf
@@ -1,14 +1,14 @@
-#resource "null_resource" "load-schema" {
-#  depends_on = [aws_rds_cluster.default]
-#
-#  provisioner "local-exec" {
-#    command = <<EOF
-#sleep 60
-#curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
-#cd /tmp
-#unzip -o mysql.zip
-#cd mysql-main
-#mysql -h ${aws_rds_cluster.default.endpoint} -u${local.RDS_USER} -p${local.RDS_PASS} <shipping.sql
-#EOF
-#  }
-#}
+resource "null_resource" "load-schema" {
+  depends_on = [aws_rds_cluster.default]
+
+  provisioner "local-exec" {
+    command = <<EOF
+sleep 60
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+cd /tmp
+unzip -o mysql.zip
+cd mysql-main
+mysql -h ${aws_rds_cluster.default.endpoint} -u${local.RDS_USER} -p${local.RDS_PASS} <shipping.sql
+EOF
+  }
+}
